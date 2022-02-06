d1529879334af441366711c33297f78413f1a5f3
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-05 22:04:58 -0800

    commit tree 593b4b83bd091487c384562a9330e565257a502e

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
+function saturdayFun (activity ="roller-skate") {
+  return (`This Saturday, I want to ${activity}!`)
+} 
+saturdayFun()
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+function mondayWork(activity="go to the office"){
+  return (`This Monday, I will ${activity}.`)
 }
-
 let wrapAdjective = function(style="*") {
   return function(adjective="special") {
     return `You are ${style}${adjective}${style}!`
